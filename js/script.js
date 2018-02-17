aed18ba8dbb82d029159519fa0c19de26052b79a
Patryk Kurzeja <patrik.kurzeja@gmail.com>
2018-02-17 22:33:39 +0100

    Add proper scripts and Fix issues

--- js/script.js
+++ js/script.js
@@ -2,73 +2,71 @@ $(document).ready(function () {
 	var scrollLink = $('.hero a');
 	scrollLink.click(function (e) {
 		e.preventDefault();
-
-		// var href = $(this).attr('href'); //attr to seter i geter
-		// var sectionPosition = $(href).offset().top-60;
-
-		$("html, body").stop().animate({ scrollTop: $(this.hash).offset().top-60 }, 1000);
-		$(window).scroll(function(){
-			var scrollbarLocation = $(this).scrollTop();
-		});
-		scrollLink.each(function(){
-			var sectionOffset = $(this.hash).offset().top;
-		})
+		$("html, body").stop().animate({
+			scrollTop: $(this.hash).offset().top - 60
+		}, 1000);
 	});
-    
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll >= 50) {
-            $("nav").addClass("fixed");
-        } else {
-            $("nav").removeClass("fixed");
-        }
 
-        $('.hidden-on-start').each(function () {
+	$(window).scroll(function () {
+		//fixed navbar
+		var scroll = $(window).scrollTop();
+		if (scroll >= 50) {
+			$("nav").addClass("fixed");
+		} else {
+			$("nav").removeClass("fixed");
+		}
+		//animated gallery -fade in on scroll
+		$('.hidden-on-start').each(function () {
 			var elementOffsetTop = $(this).offset().top - 600;
-
-			// var f = function () {};
-
-			// $(this).click(f);
-			// this.addEventListener('click', f, true);
-
 			if (scroll > elementOffsetTop) {
 				$(this).addClass('visible');
 			}
 		});
-    });
-
-    
-    
-    var imagesArray = $('.tab-image img').toArray();
-    function showImage(number) {
-		var url = $(imagesArray[number]).attr('src');
-
-    	$('#modal').fadeIn('slow', function(){
-		$(this).show();
-		})
-    	$('#modal-image').attr('src', url);
-    	if (number >= imagesArray.length-1) {
-    		$('#modal-next').hide();
-    	} else {
-    		$('#modal-next').data('toNumber', number + 1).show();
-		}
-		if (number <= 0) {
-    		$('#modal-prev').hide();
-    	} else {
-    		$('#modal-prev').data('toNumber', number - 1).show();
-		}
-    }
-
-    $('#modal-next').click(function () {
-    	showImage($(this).data('toNumber'));
-    });
-
-	$('#modal-prev').click(function () {
-    	showImage($(this).data('toNumber'));
+		//active watching on navbar links
+		var scrollbarLocation = $(this).scrollTop();
+		scrollLink.each(function () {
+			var sectionOffset = $(this.hash).offset().top - 60;
+			if (sectionOffset <= scrollbarLocation) {
+				$(this).parent().addClass('active');
+				$(this).parent().siblings().removeClass('active');
+			}
+		});
 	});
 
-	$('#modal-close').click(function(){
-		$('#modal').fadeOut('slow', function(){
+
+
+
+	var imagesArray = $('.tab-image img').toArray();
+
+	function showImage(number) {
+		var url = $(imagesArray[number]).attr('src');
+
+		$('#modal').fadeIn('slow', function () {
+			$(this).show();
+		})
+		$('#modal-image').attr('src', url);
+		if (number >= imagesArray.length - 1) {
+			$('#modal-next').hide();
+		} else {
+			$('#modal-next').data('toNumber', number + 1).show();
+		}
+		if (number <= 0) {
+			$('#modal-prev').hide();
+		} else {
+			$('#modal-prev').data('toNumber', number - 1).show();
+		}
+	}
+
+	$('#modal-next').click(function () {
+		showImage($(this).data('toNumber'));
+	});
+
+	$('#modal-prev').click(function () {
+		showImage($(this).data('toNumber'));
+	});
+
+	$('#modal-close').click(function () {
+		$('#modal').fadeOut('slow', function () {
 			$(this).hide();
 		})
 	})
@@ -78,37 +76,37 @@ $(document).ready(function () {
 		showImage(clickedNumber);
 	});
 
-	$('#seeMoreBtn').click(function(event){
+	$('#seeMoreBtn').click(function (event) {
 		event.preventDefault();
 		$('#grid-to-show').toggleClass('displayed-grid');
 		$('.js-parent-grid').slideToggle(500);
 
 		var seeMore = $('#seeMoreBtn');
-				
+
 		if ($('#grid-to-show').hasClass('displayed-grid')) {
-				seeMore.text('see less');
-				$('.fa-angle-down').addClass('rotation');
+			seeMore.text('see less');
+			$('.fa-angle-down').addClass('rotation');
 		} else {
 			seeMore.text('see more');
-				$('.fa-angle-down').removeClass('rotation');
+			$('.fa-angle-down').removeClass('rotation');
 		}
-		
+
 	});
-
-// 	$('#seeMoreBtn').click(function(event){
-// 		event.preventDefault();
-// 				$('#grid-to-show').slideToggle(500).css('display', 'grid');
-
+	//----otwarcie jednego z faq---------
+	$('.faq-button, .faq-name').click(function (event) {
+		event.preventDefault();
+		var thisFaq = $(this).closest('.faq-item');
 		
+		if (thisFaq.hasClass('active')) {
+			
+			$('.faq-answer', thisFaq).slideToggle(function () {
+				thisFaq.removeClass('active');
 
-
-
+			});
+		} else {
+			
+			thisFaq.addClass('active');
+			$('.faq-answer', thisFaq).slideToggle();
+		}
+	});
 });
-
-
-
-
-
-
-
-
